d6caabcb00b89841b5f7df7a3083bc48cd815dab
ijharulislam <ijharalkawsary@gmail.com>
2019-05-08 20:20:58 +0600

    Todo app with Vue js

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+   Filter Todos
+   <select @change="filterTodos($event)">
+      <option value="200">200</option>
+      <option value="100">100</option>
+      <option value="50">50</option> 
+      <option value="20">20</option>
+      <option value="10">10</option>
+   </select>
+</div>
+
+</template>
+
+
+<script>
+
+import {mapActions} from 'vuex';
+
+export default {
+  name: "Filter Todos",
+  methods: {
+    ...mapActions(["filterTodos"])
+ }
+
+}
+</script>
+
+<style scoped>
+   select {
+      margin-top: 20px;
+      padding: 6px;
+      border: 1px solid #41b883;
+   }
+</style>
